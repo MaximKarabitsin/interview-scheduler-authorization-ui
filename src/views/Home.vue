91480fdb5a38758d52b2b72ea8051b4e24abafba
MaximKarabitsin <48644995+MaximKarabitsin@users.noreply.github.com>
2019-12-15 22:38:01 +0400

    Edit home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,22 @@
 <template>
   <div>
-    Home page
+    <h1 class="home-title">Authorization service</h1>
+    <p class="home-text">Training project</p>
   </div>
 </template>
 
 <script>
 export default {};
 </script>
+
+<style>
+.home-title {
+  text-align: center;
+  font-weight: normal;
+  margin-top: 40px;
+}
+
+.home-text {
+  text-align: center;
+}
+</style>
